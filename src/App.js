65bcfe12959ab6786d36d4bf625150c72fee6849
Hina0222 <hina0222@naver.com>
2024-09-27 23:32:01 +0900

    초기세팅

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { ReactComponent as Test } from './assets/Test.svg'
+
+function App() {
+  return (
+    <div>
+      <Test fill='black' />
+    </div>
+  );
+}
+
+export default App;
